c926304263554ea9498a1c74ec9a201c411188d1
Alexandre Silva <xande.xas@gmail.com>
2025-03-01 17:22:59 +0000

    removendo require model

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,17 +20,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   avatar: {
     type: String,
-    required: true,
+
     validate: {
       validator: (v) => validator.isURL(v),
       message: "link invalido",

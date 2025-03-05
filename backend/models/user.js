a9c321e2b58fa23d2a9fa31512cd620792d5238f
Alexandre Silva <xande.xas@gmail.com>
2025-03-05 16:50:31 +0000

    add defaul

--- backend/models/user.js
+++ backend/models/user.js
@@ -18,16 +18,20 @@ const userSchema = new mongoose.Schema({
 
   name: {
     type: String,
+    default: "Jacques Cousteau",
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
+    default: "Explorer",
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg",
 
     validate: {
       validator: (v) => validator.isURL(v),

0c48184539deab94e6003b9d5be7f439185fc19c
Alexandre Silva <xande.xas@gmail.com>
2025-03-05 20:58:58 +0000

    teste console.log

--- frontend/src/components/Main/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/Main/components/ProtectedRoute/ProtectedRoute.jsx
@@ -6,6 +6,7 @@ export default function ProtectedRoute({ children, anonymous = false }) {
   const location = useLocation();
   const from = location.state?.from || "/";
   const { isLoggedIn } = useContext(AppContext);
+  console.log(isLoggedIn, anonymous);
 
   if (anonymous && isLoggedIn) {
     return <Navigate to={from} />;
